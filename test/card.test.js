88895304c06873e42cb4708610312f65de8cb9c8
Ben Titcomb <btitcomb@scpr.org>
2018-01-28 12:19:08 -0800

    Completely replace old stirng interpolation with Handlebars and helpers.

--- test/card.test.js
+++ test/card.test.js
@@ -2,17 +2,11 @@
 
 const assert      = require('assert');
 const card        = require('../lib/builders/card');
+const generateContext = require('../lib/context-generator');
+
 
 describe('operators', function(){
-  let context = {
-    attributes: {
-    },
-    response: {
-
-    },
-    responseBody: {}
-  };
-
+  let context = generateContext();
   it('adds a simple card object to the response', function(){
     let cardInput = {title: "hello", content: "hello world"};
     card(cardInput, context);
--- test/eval-string.test.js
+++ test/eval-string.test.js
@@ -2,28 +2,20 @@
 
 const assert      = require('assert');
 const evalString  = require('../lib/eval-string');
+const generateContext = require('../lib/context-generator');
 
 describe('operators', function(){
-  let context = {
-    attributes: {
-    }
-  };
+  let context = generateContext();
 
-  it('processes template strings', function(){
-    let evaluation = evalString('The result is ${1 + 2}.', context);
+  it('can do basic math', function(){
+    let evaluation = evalString('The result is {{add 1 2}}.', context);
     assert.equal(evaluation, 'The result is 3.');
   });
 
   it('replaces with empty strings when undefined/null/NaN are passed', function(){
-    let evaluation = evalString('This is a ${undefined} string.', context);
+    let evaluation = evalString('This is a {{someUndefinedvariable}} string.', context);
     assert.equal(evaluation, 'This is a  string.');
   });
-
-  it('formats date strings to readable format', function(){
-    let evaluation = evalString('It is currently ${new Date()}', context);
-    let match      = evaluation.match(/\d{1,2} \d{1,2} [am|pm]{2} on [A-z]+ \d{1,2} \d{4}/) ? true : false;
-    assert.equal(match, true);
-  })
   
 });
 
