10ab3b6287d1147927b10fa18b0cfaa57caca6f5
Ben Titcomb <btitcomb@scpr.org>
2018-02-03 11:21:35 -0800

    Add repeat option.  Not too happy about it, but it works.

--- test/runtime.test.js
+++ test/runtime.test.js
@@ -5,6 +5,8 @@ const assert            = require('chai').assert;
 const generateContext   = require('../lib/context-generator');
 // const helloWorldRequest = require('./mocks/requests/hello-world');
 const runtime         = require('../lib/runtime');
+const foobarIntentRequest = require('./mocks/requests/foobar-intent');
+
 
 
 
@@ -51,6 +53,70 @@ describe('runtime', function(){
         done();
       });
   });
+
+
+  it('honors the repeat option', (done) => {
+    let scenes = {
+      entrypoint: {
+        'label 1': {
+          say: 'hello',
+          ask: 'baz?',
+          intents: {
+            FoobarIntent: {
+              repeat: null
+            }
+          }
+        },
+        'label 2': {
+          say: 'cruel',
+          'go to': 'label 3'
+        },
+        'label 3': {
+          say: 'world',
+          'go to': 'label 4'
+        }
+      }
+    };
+    let context = generateContext(foobarIntentRequest,null,scenes);
+    runtime(context, scenes)
+      .then(context => {
+        assert.equal(context.get('navigator.currentLabelName'), 'label 1');
+        assert.ok(context.get('responseBody.outputSpeech.ssml').match('hello'));
+        done();
+      });
+  });
+
+
+  it('repeats a question only without the main speech or cards', (done) => {
+    let scenes = {
+      entrypoint: {
+        'label 1': {
+          say: 'hello',
+          ask: 'baz?',
+          intents: {
+            FoobarIntent: {
+              repeat: 'question'
+            }
+          }
+        }
+      }
+    };
+    let context = generateContext(foobarIntentRequest,null,scenes);
+    context.set('sessionAttributes.WAITING', true);
+    context.set('sessionAttributes.LABEL', 'label 1');
+    runtime(context, scenes)
+      .then(context => {
+        assert.equal(context.get('navigator.currentLabelName'), 'label 1');
+        assert.notOk(context.getWithDefault('responseBody.outputSpeech.ssml', '').match('hello'));
+        assert.ok(context.get('responseBody.outputSpeech.ssml').match('baz?'));
+        done();
+      });
+  });
+
+
+
+
+
 
 });
 
